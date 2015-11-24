73518fc5c048e809d560dafe970a16221762ff2a
Pau Torres <ptmonlinework@gmail.com>
2015-11-24 09:46:21 +0100

    changed functionality on controller

--- myapp2/controllers/controller.js
+++ myapp2/controllers/controller.js
@@ -3,8 +3,7 @@ var cheerio = require('cheerio');
 var firstHeader;
 var thealbumarray = [];
 var theartistarray = [];
-
-
+var count = 0;
 var thealbumarray = [];
 var model = require('../models/model');
 
@@ -16,8 +15,6 @@ exports.init= function(req, res){
   count = 0;
 }
 exports.getter = function(req, res){
-  console.log(req.body.albumtitle);
-    console.log(req.body.albumartist);
     for (var x = 0; x < req.body.albumtitle.trim().split(' ').length; x++){
       if(req.body.albumtitle.split(' ')[x] == 'and' || req.body.albumtitle.split(' ')[x] == 'of' || req.body.albumtitle.split(' ')[x] == 'to' || req.body.albumtitle.split(' ')[x] == 'in'){
         thealbumarray.push(req.body.albumtitle.split(' ')[x].toLowerCase());
@@ -33,9 +30,32 @@ exports.getter = function(req, res){
     var titleArtist = theartistarray.toString().replace(/,/g, "_");
     console.log('the title album is : ' + titleAlbum);
     console.log('... and the artist is ' + titleArtist);
-    model.checkString(titleAlbum, titleArtist, function(err, callback){
-        console.log(callback)
-        res.render('users', { titleAlbum: 'howdy', data: callback});
+    var finishstring = titleAlbum + "_(" + titleArtist+"_album)"
+    checkString(titleAlbum, titleArtist)
+    function checkString(titleAlbum, titleArtist){
+    count;
+    // if(titleArtist == undefined || titleArtist == 'undefined' || titleArtist ==null ){
+    if(count == 1){
+      finishString = titleAlbum + '_(album)';
+    }else if(count == 2){
+      finishString = titleAlbum
+    }else if(count == 3){
+      console.log('sorry! no match found');
+      firstHeader = 'Sorry no criteria found!'
+      res.render('users', { titleAlbum: firstHeader});
+      return false;
+    }else{
+      finishString = titleAlbum + '_(' + titleArtist +'_album)';
+    }
+   model.searchItems(finishString, function(err, callback){
+        console.log(err)
+        console.log('the call back is : ' + Object.keys(callback).length)
+        if(Object.keys(callback).length){
+          res.render('users', { titleAlbum: req.body.albumtitle, data: callback});
+        }else{
+          res.render('users', { titleAlbum: req.body.albumtitle, data: 'Sorry no album found!'});
+        }
 
     })
+  }
 }
--- myapp2/models/model.js
+++ myapp2/models/model.js
@@ -3,28 +3,7 @@ var cheerio = require('cheerio');
 var count = 0;
 var datainformation = [];
 var firstHeader;
-var ctrl = require('../controllers/controller.js')
-exports.checkString = function(titleAlbum, titleArtist, cb) {
-	function checkcheck(titleAlbum, titleArtist){
-	var finishstring = titleAlbum + "_(" + titleArtist+"_album)"
-	count;
-    // if(titleArtist == undefined || titleArtist == 'undefined' || titleArtist ==null ){
-    if(count == 1){
-      finishString = titleAlbum + '_(album)';
-    }else if(count == 2){
-      finishString = titleAlbum
-    }else if(count == 3){
-      console.log('sorry! no match found');
-      firstHeader = 'Sorry no criteria found!'
-      cb(null, 'Sorry!')
-      // res.render('users', { titleAlbum: titleArtist});
-      return false;
-    }else{
-      finishString = titleAlbum + '_(' + titleArtist +'_album)';
-    }
-    searchItems(finishString)
-	};
-	function searchItems (finishString){
+exports.searchItems = function(finishString, cb){
 	    request({
 		    uri: "https://en.wikipedia.org/wiki/" + finishString,
 		  }, function(error, response, body) {
@@ -32,10 +11,10 @@ exports.checkString = function(titleAlbum, titleArtist, cb) {
 		    firstHeader = $('.firstHeading').text();
 		    var	title = $('.wikitable.infobox').find('td > a');
 		    var ititle = $('.wikitable.infobox').find('td > i > a');
-		    if($('#noarticletext').length == 1){
-		      count += 1;
-		      checkcheck(titleAlbum)
-		    }else{
+		    // if($('#noarticletext').length == 1){
+		    //   count += 1;
+		    //   checkString(titleAlbum)
+		    // }else{
 			    $(title).each(function(){
 			      	var punctuation = $(this).parent().next().find('span').attr('title')
 			        var wordPunctuation = $(this).parent().next().text();
@@ -55,9 +34,10 @@ exports.checkString = function(titleAlbum, titleArtist, cb) {
 			        }
 
 			      })
-		 	}
+		 	// }
+		 	console.log('datainformation is : ' + typeof(datainformation))
+		 	console.log('datainformation is too : ' + datainformation['Metacritic'])
 		 	cb(null, datainformation)
 		 });
-		 };
-	checkcheck(titleAlbum, titleArtist);
-	};
+	}
+		
